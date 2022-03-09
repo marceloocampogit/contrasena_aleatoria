5b8ba3546184aca24a51b00e5841c5dcb2f014c2
titooooooooooooooooo <marceloocampo1993@icloud.com>
2022-03-09 13:35:08 -0300

    Primer envio a master

---
+++ contrasena.py
@@ -0,0 +1,27 @@
+import random as rd
+
+def generar_contrasena(cantidad):
+    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+    caracteres = MAYUS + MINUS + NUMS + CHARS
+
+    contrasena = []
+
+    for i in range(cantidad):
+        caracter_random = rd.choice(caracteres)
+        contrasena.append(caracter_random)
+
+    contrasena = "".join(contrasena)
+    return contrasena
+
+def run():
+    c_contrasena = int(input("Indica cuantos caracteres necesitas para la contraseña: "))
+    contrasena = generar_contrasena(c_contrasena)
+    print('Tu nueva contraseña es: ' + contrasena)
+
+
+if __name__ == '__main__':
+    run() 
